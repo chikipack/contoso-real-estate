3096e882e45e19babe4cb97628a7f5c30ad6d1aa
Victor Alejandro Anaya Martinez <49319174+chikipack@users.noreply.github.com>
2023-08-12 07:37:38 +0000

    docs: new config

--- packages/docs/website/docusaurus.config.js
+++ packages/docs/website/docusaurus.config.js
@@ -9,12 +9,14 @@ const config = {
   // ------- Required Fields -------
   title: 'Contoso Real Estate: Developer Guide',
   url: 'https://azure-samples.github.io',
-  baseUrl: '/',  // set this to '/contoso-real-estate/' for GitHub pages deployment
+  baseUrl: '/contoso-real-estate/',  // set this to '/contoso-real-estate/' for GitHub pages deployment
 
   // ------- Optional: deploy related -------
   organizationName: 'Azure-Samples',  // needed only for GitHub Pages deployment
   projectName: 'contoso-real-estate', // -- ditto --
   deploymentBranch: "gh-pages",
+
+  trailingSlash: false,
 
   //------- Optional: build checks -----
   onBrokenLinks: 'throw',
